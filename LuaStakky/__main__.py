36d6d685a63d0751560e3e542840a4ecbef2d76c
artem3213212 <cx-1212@yandex.ru>
2020-04-27 18:06:39 +0300

    Minimal cli

--- LuaStakky/__main__.py
+++ LuaStakky/__main__.py
@@ -1,11 +1,11 @@
-import LuaStakky,os
+import LuaStakky, os, sys
+from argparse import ArgumentParser
 
-App=LuaStakky.StakkyApp(path=os.getcwd())
-App.build()
-
-
-
-
-'''
-
-'''
+App = LuaStakky.StakkyApp(path=os.getcwd())
+parser = ArgumentParser()
+parser.add_argument('-P', '--profile')
+args = parser.parse_args()
+if args.profile:
+    App.build(profile=args.profile)
+else:
+    App.build()
